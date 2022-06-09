08e8ca58c2893a975a78c827cd04bd4ea6c87096
cesarromerodev <cesar_romero.dev@outlook.com>
2022-06-09 17:55:22 -0500

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+
+## [JAVASCRIPT MODERNO UDEMY]
+
+### [Introducción a JavaScript]
+
+    - Inicio[09-06-2022]
+        - Depuración de BreakPoints
+        - Consola
+        - Prompt, confirm, alert
---
+++ IntroducciónJavaScript/depuracionBreakPoints.js
@@ -0,0 +1,15 @@
+let = a = 10;
+      b = 20;
+      c = 30;
+      d = 'Hola';
+      e = 50;
+      f = 'Spiderman'
+
+      console.log('%c Mis variables', 'color:blue; font-weigth: bold');     //para integrar style
+      console.log({a});
+      console.log({b});
+      console.log({c});
+
+      console.table({a, b, c, d, e, f});
+      
+      c = 'HOLA DENUEVO';
